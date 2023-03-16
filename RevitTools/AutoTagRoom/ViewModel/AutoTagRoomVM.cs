77a7f6695d4d0deaa2e63f852503cdbf2bc262c9
phamninh704 <phamninh704@gmail.com>
2023-03-16 23:22:32 +0700

    cn

--- RevitTools/AutoTagRoom/ViewModel/AutoTagRoomVM.cs
+++ RevitTools/AutoTagRoom/ViewModel/AutoTagRoomVM.cs
@@ -1,40 +1,104 @@
-﻿using Autodesk.Revit.UI;
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
 using CommunityToolkit.Mvvm.ComponentModel;
-using System;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace RevitTools
 {
-    public class AutoTagRoomVM : ObservableObject
+    public partial class AutoTagRoomVM : ObservableObject
     {
         private AutoTagRoomView _view;
-        public AutoTagRoomView view {
+        public AutoTagRoomView view
+        {
             get
             {
-                if(_view == null)
-                    _view = new AutoTagRoomView() { DataContext=this};
+                if (_view == null)
+                    _view = new AutoTagRoomView() { DataContext = this };
                 return _view;
-            } 
+            }
             set
             {
                 _view = value;
                 OnPropertyChanged();
             }
         }
+        [ObservableProperty]
+        public ObservableCollection<RoomTagType> srcroomName = new ObservableCollection<RoomTagType>();
+        [ObservableProperty]
+        private ObservableCollection<RoomTagType> srcroomWall = new ObservableCollection<RoomTagType>();
+        [ObservableProperty]
+        private ObservableCollection<RoomTagType> srcroomFloor = new ObservableCollection<RoomTagType>();
+        [ObservableProperty]
+        private ObservableCollection<RoomTagType> srcroomCeiling = new ObservableCollection<RoomTagType>();
+        [ObservableProperty]
+        private ObservableCollection<RoomTagType> srcroomHabaki = new ObservableCollection<RoomTagType>();
+        [ObservableProperty]
+        private ObservableCollection<RoomTagType> srcroomConnectCeiling = new ObservableCollection<RoomTagType>();
+
+        [RelayCommand]
+        void Run()
+        {
+            MessageBox.Show("Test");
+        }
+        [RelayCommand]
+        void Check()
+        {
+            if(view.rbtTruss.IsChecked == true)
+            {
+                MessageBox.Show("11");
+            }
+            else
+            {
+                MessageBox.Show("22");
+            }
+        }
         private UIDocument uidoc;
-
-
         public AutoTagRoomVM(UIDocument uidoc)
         {
-            var source = GetData.instance.GetAllTagRoom(uidoc.Document);
-            MessageBox.Show(source.Count.ToString());
-            view.cbtagRoom.ItemsSource = source;
-            view.cbtagRoom.DisplayMemberPath = "Name";
+            List<RoomTagType> source = new List<RoomTagType>(GetData.instance.GetAllTagRoom(uidoc.Document));
+            if (true)
+            {
+                SetItemSource(source, true);
+            }
             this.uidoc = uidoc;
+        }
+
+        void SetItemSource(List<RoomTagType> source, bool b)
+        {
+            if (b)
+            {
+                SrcroomName = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_一般").Cast<RoomTagType>().ToList());
+
+                SrcroomWall = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_壁").Cast<RoomTagType>().ToList());
+
+                SrcroomCeiling = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_天井").Cast<RoomTagType>().ToList());
+
+                SrcroomFloor = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_床").Cast<RoomTagType>().ToList());
+
+                SrcroomHabaki = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_幅木").Cast<RoomTagType>().ToList());
+
+                SrcroomConnectCeiling = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_見切縁").Cast<RoomTagType>().ToList());
+            }
+            else
+            {
+                SrcroomName = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_一般").Cast<RoomTagType>().ToList());
+
+                SrcroomWall = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_壁_Truss対応").Cast<RoomTagType>().ToList());
+
+                SrcroomCeiling = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_天井_Truss対応").Cast<RoomTagType>().ToList());
+
+                SrcroomFloor = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_床_Truss対応").Cast<RoomTagType>().ToList());
+
+                SrcroomHabaki = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_幅木_Truss対応").Cast<RoomTagType>().ToList());
+
+                SrcroomConnectCeiling = new ObservableCollection<RoomTagType>(source.Where(p => p.FamilyName == "dタグ_部屋_見切縁_Truss対応").Cast<RoomTagType>().ToList());
+            }
         }
     }
 }
---
+++ RevitTools/Database/GetData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+
+namespace RevitTools
+{
+    public class GetData
+    {
+        private GetData() { }
+        public static GetData instance { get; } = new GetData();
+        public List<RoomTagType> GetAllTagRoom(Document doc)
+        {
+            try
+            {
+                
+                List<RoomTagType> result = new FilteredElementCollector(doc).OfClass(typeof(FamilySymbol))
+                        .Where(p => (p as FamilySymbol).Category.Name == "Room Tags").Cast<RoomTagType>().ToList();
+                return result;
+            }
+            catch (System.Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            return null;
+        }
+    }
+}
